6131d566fb0c873283ba9e065ebcfbc072246596
Jia-Yin <jywglady@gmail.com>
2022-04-13 16:36:10 +0800

    Add JS Function

--- myscripts.js
+++ myscripts.js
@@ -1,2 +1,17 @@
 
 document.getElementById("demo").innerHTML = 5 + 6;
+console.log("The answer should be 11")
+window.alert("視窗訊息")
+
+var a = 0;
+
+function myFunction() {
+  if (a==0) {
+    document.getElementById("demo").style.fontSize = '35px';
+    a = 1;
+  }
+  else {
+    document.getElementById("demo").style.fontSize = '15px';
+    a = 0;
+  }
+}
